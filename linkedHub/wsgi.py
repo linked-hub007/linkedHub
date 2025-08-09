7bd15f8a88e468149137b6ab09f21060d5777a6a
SOMU3103 <somnathp02006@gmail.com>
2025-08-09 11:50:50 +0530

    mm

--- linkedHub/wsgi.py
+++ linkedHub/wsgi.py
@@ -1,10 +1,16 @@
-# wsgi.py
+"""
+WSGI config for linkedHub project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
 import os
+
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'linkedHub.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
