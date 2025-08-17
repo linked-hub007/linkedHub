fe6a61782dc4ecbe29f3afb12cfdb9cb032c781b
SOMU3103 <somnathp02006@gmail.com>
2025-08-17 11:24:53 +0530

    so

--- linkedHub/settings.py
+++ linkedHub/settings.py
@@ -40,6 +40,7 @@ if not DEBUG:
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 #-----------------------------------------------------------
 PASSWORD_HASHERS = [
