b10c46427440a876a922b5a878271d763b7e09c9
SOMU3103 <somnathp02006@gmail.com>
2025-08-22 19:38:28 +0530

    sett

--- linkedHub/urls.py
+++ linkedHub/urls.py
@@ -2,19 +2,24 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('shop.urls')),
-    
 ]
 
-# Serve media files in development
+# Serve static and media files
 if settings.DEBUG:
+    # Development - serve both static and media files
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
-    # For production on Render, you need to serve media files through Django
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    # Production - serve media files through Django (WhiteNoise handles static files)
+    urlpatterns += [
+        re_path(r'^images/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
